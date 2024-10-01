66e7a2681ecb457f37cbd573376786308d51d8fc
Gary Hsu <bghgary@users.noreply.github.com>
2024-10-01 16:32:08 -0700

    Update logo and add restart file explorer feature to main window

--- Source/glTF/UI/MainWindow.xaml.cs
+++ Source/glTF/UI/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
+using System;
+using System.Diagnostics;
 
 namespace glTF
 {
@@ -8,6 +12,34 @@ namespace glTF
         {
             this.InitializeComponent();
             this.ApplySettings();
+        }
+
+        private async void Hyperlink_Click(Hyperlink sender, HyperlinkClickEventArgs args)
+        {
+            var dialog = new ContentDialog()
+            {
+                XamlRoot = this.Content.XamlRoot,
+                Title = "Restart the File Explorer?",
+                Content = "Doing this will close all open File Explorer windows.",
+                PrimaryButtonText = "Yes",
+                CloseButtonText = "No",
+                DefaultButton = ContentDialogButton.Primary,
+            };
+
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+            {
+                KillExplorer();
+            }
+        }
+
+        private static void KillExplorer()
+        {
+            var processes = Process.GetProcessesByName("explorer");
+            foreach (var process in processes)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
         }
     }
 }
